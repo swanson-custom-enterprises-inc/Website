ef26003a13290d5cb62f74e76da9ebab086068e3
swanson-custom-enterprises-inc <swanson.custom.enterprises@gmail.com>
2026-03-06 18:28:52 -0600

    Load HTML content dynamically using data-include

--- js/include.js
+++ js/include.js
@@ -1,1 +1,7 @@
-
+document.querySelectorAll("[data-include]").forEach(el => {
+  fetch(el.getAttribute("data-include"))
+    .then(response => response.text())
+    .then(data => {
+      el.innerHTML = data;
+    });
+});
