c3fe94056c50474466d9060091203eead4658c98
swanson-custom-enterprises-inc <swanson.custom.enterprises@gmail.com>
2026-05-22 15:23:56 -0500

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,40 +1,12 @@
 function loadComponent(id,file){
 
 fetch(file)
-
-.then(response => response.text())
-
-.then(data => {
-
+.then(response=>response.text())
+.then(data=>{
 document.getElementById(id).innerHTML=data;
-
-/* -------------------------
-NAVIGATION SETUP
-------------------------- */
-
-if(id==="nav-placeholder"){
-
-const menuButton=document.getElementById("menuButton");
-
-const sidebar=document.getElementById("sidebar");
-
-if(menuButton && sidebar){
-
-menuButton.addEventListener("click",()=>{
-
-sidebar.classList.toggle("open");
-
 });
 
 }
-
-}
-
-});
-
-}
-
-/* LOAD COMPONENTS */
 
 loadComponent("nav-placeholder","components/nav.html");
 
